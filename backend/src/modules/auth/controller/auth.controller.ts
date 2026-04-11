1c20ddae2be27e176bac1c81b617544a88507630
Jeet <jeetnath2110@gmail.com>
2026-04-11 13:33:32 +0530

    folder structure changes

--- backend/src/modules/auth/controller/auth.controller.ts
+++ backend/src/modules/auth/controller/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
-  @Get('signin')
+  @Post('signin')
   signin() {
     return 'signin';
   }
