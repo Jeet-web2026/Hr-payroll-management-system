3fcb9db0ada08435a4e9f51a3be258ee33e3df3d
Jeet <jeetnath2110@gmail.com>
2026-04-11 17:06:02 +0530

    authentication going on

--- backend/src/modules/auth/controller/auth.controller.ts
+++ backend/src/modules/auth/controller/auth.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, HttpCode, HttpException, Post } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
+import { UsersService } from '../../users/services/users.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly userService: UsersService) {}
+
   @Post('signin')
   @HttpCode(200)
   signin() {
