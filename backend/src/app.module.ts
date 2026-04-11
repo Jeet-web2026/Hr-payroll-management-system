b5870cf58d029dc5338fd73b7b32ff6fd2948f79
Jeet <jeetnath2110@gmail.com>
2026-04-11 12:13:57 +0530

    modules preparing done

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, DashboardModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
