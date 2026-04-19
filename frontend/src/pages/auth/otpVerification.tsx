d8ea90a76c8713a4a02aaa1975e7ec8e07e7d2ce
Jeet <jeetnath2110@gmail.com>
2026-04-19 13:51:54 +0530

    otp verification changes

--- frontend/src/pages/auth/otpVerification.tsx
+++ frontend/src/pages/auth/otpVerification.tsx
@@ -1,13 +1,16 @@
 import { GuestLayout } from "@/comon/guestLayout"
-import { useEffect, useState } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { useSearchParams } from "react-router-dom"
 import logo from "@/assets/images/logo.png";
+import { toast } from "sonner";
+import apiService from "@/comon/api/apiService";
 
 const OtpVerification = () => {
     const [searchParam] = useSearchParams();
     const otpFromUrl = searchParam.get('otp');
 
     const [otpInputs, setOtpInputs] = useState<string[]>(Array(6).fill(""));
+    const [submitting, setsubmitting] = useState(false)
 
     useEffect(() => {
         if (otpFromUrl) {
@@ -26,12 +29,30 @@ const OtpVerification = () => {
         }
     };
 
+    const verificationForm = async (e: any) => {
+        e.preventDefault();
+
+        try {
+            const data = await apiService.post("/otp-verification");
+            console.log(data);
+            toast.success("Verfication successfull!", {
+                position: "top-right",
+                richColors: true
+            });
+        } catch (error) {
+            toast.error("Something Went wrong!", {
+                position: "top-right",
+                richColors: true
+            });
+        }
+    }
+
     return (
         <GuestLayout>
             <div className="lg:w-[35%] bg-zinc-900 p-10 rounded flex flex-col gap-8">
                 <img src={logo} alt="logo" className="w-18 mx-auto" />
                 <p className="text-center text-xl">Please verify your Email</p>
-                <form action="" method="post" className="flex flex-col gap-5">
+                <form onSubmit={verificationForm} method="post" className="flex flex-col gap-5">
                     <div className="flex flex-row gap-2 justify-center items-center">
                         {otpInputs.map((_, index) => (
                             <input
