918789f9778df408c1992da19df9361563594ec1
Jeet <jeetnath2110@gmail.com>
2026-04-19 22:59:23 +0530

    access token and refresh token related development

--- frontend/src/comon/api/axiosInstance.ts
+++ frontend/src/comon/api/axiosInstance.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
+import { TokenService } from "./tokenService";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
   },
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = TokenService.get();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -20,10 +22,18 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      window.location.href = "/";
+      try {
+        const res = await axiosInstance.post("/auth/refresh");
+        TokenService.set(res.data.data.accessToken);
+
+        error.config.headers.Authorization = `Bearer ${res.data.data.accessToken}`;
+        return axiosInstance(error.config);
+      } catch {
+        TokenService.clear();
+        window.location.href = "/";
+      }
     }
     return Promise.reject(error);
   },
