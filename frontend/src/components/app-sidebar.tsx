8efdb466b65c0365e88762ffe0c2d3c585ffd68a
Jeet <jeetnath2110@gmail.com>
2026-05-02 02:09:34 +0530

    fetching user on dashboard done

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -14,167 +14,203 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { LayoutDashboardIcon, ListIcon, ChartBarIcon, FolderIcon, UsersIcon, CameraIcon, FileTextIcon, Settings2Icon, CircleHelpIcon, SearchIcon, DatabaseIcon, FileChartColumnIcon, FileIcon, CommandIcon } from "lucide-react"
-
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "#",
-      icon: (
-        <LayoutDashboardIcon
-        />
-      ),
-    },
-    {
-      title: "Lifecycle",
-      url: "#",
-      icon: (
-        <ListIcon
-        />
-      ),
-    },
-    {
-      title: "Analytics",
-      url: "#",
-      icon: (
-        <ChartBarIcon
-        />
-      ),
-    },
-    {
-      title: "Projects",
-      url: "#",
-      icon: (
-        <FolderIcon
-        />
-      ),
-    },
-    {
-      title: "Team",
-      url: "#",
-      icon: (
-        <UsersIcon
-        />
-      ),
-    },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: (
-        <CameraIcon
-        />
-      ),
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: (
-        <FileTextIcon
-        />
-      ),
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: (
-        <FileTextIcon
-        />
-      ),
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: (
-        <Settings2Icon
-        />
-      ),
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: (
-        <CircleHelpIcon
-        />
-      ),
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: (
-        <SearchIcon
-        />
-      ),
-    },
-  ],
-  documents: [
-    {
-      name: "Data Library",
-      url: "#",
-      icon: (
-        <DatabaseIcon
-        />
-      ),
-    },
-    {
-      name: "Reports",
-      url: "#",
-      icon: (
-        <FileChartColumnIcon
-        />
-      ),
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: (
-        <FileIcon
-        />
-      ),
-    },
-  ],
-}
+import apiService from "@/comon/api/apiService"
+import { toast } from "sonner"
+import { Skeleton } from "./ui/skeleton"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  type User = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    ipAddress: string;
+    isEmailVerified: boolean;
+    lastLogin: string;
+    phone: string;
+    profilePicture: string;
+    role: string;
+    status: string;
+  }
+
+
+  const [user, setUser] = React.useState<User | null>(null);
+  const [pending, setPending] = React.useState(false);
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        setPending(true);
+
+        const res = await apiService.get('/user/me');
+        setUser(res.data.data);
+      } catch (error) {
+        toast.error("Failed to fetch user", { position: "top-right", richColors: true });
+      } finally {
+        setPending(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+  const data = {
+    user: {
+      name: user ? user.firstName + " " + user.lastName : "",
+      email: user ? user.email : "",
+      avatar: user ? user.profilePicture : '',
+    },
+    navMain: [
+      {
+        title: "Dashboard",
+        url: "#",
+        icon: (
+          <LayoutDashboardIcon
+          />
+        ),
+      },
+      {
+        title: "Lifecycle",
+        url: "#",
+        icon: (
+          <ListIcon
+          />
+        ),
+      },
+      {
+        title: "Analytics",
+        url: "#",
+        icon: (
+          <ChartBarIcon
+          />
+        ),
+      },
+      {
+        title: "Projects",
+        url: "#",
+        icon: (
+          <FolderIcon
+          />
+        ),
+      },
+      {
+        title: "Team",
+        url: "#",
+        icon: (
+          <UsersIcon
+          />
+        ),
+      },
+    ],
+    navClouds: [
+      {
+        title: "Capture",
+        icon: (
+          <CameraIcon
+          />
+        ),
+        isActive: true,
+        url: "#",
+        items: [
+          {
+            title: "Active Proposals",
+            url: "#",
+          },
+          {
+            title: "Archived",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Proposal",
+        icon: (
+          <FileTextIcon
+          />
+        ),
+        url: "#",
+        items: [
+          {
+            title: "Active Proposals",
+            url: "#",
+          },
+          {
+            title: "Archived",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Prompts",
+        icon: (
+          <FileTextIcon
+          />
+        ),
+        url: "#",
+        items: [
+          {
+            title: "Active Proposals",
+            url: "#",
+          },
+          {
+            title: "Archived",
+            url: "#",
+          },
+        ],
+      },
+    ],
+    navSecondary: [
+      {
+        title: "Settings",
+        url: "#",
+        icon: (
+          <Settings2Icon
+          />
+        ),
+      },
+      {
+        title: "Get Help",
+        url: "#",
+        icon: (
+          <CircleHelpIcon
+          />
+        ),
+      },
+      {
+        title: "Search",
+        url: "#",
+        icon: (
+          <SearchIcon
+          />
+        ),
+      },
+    ],
+    documents: [
+      {
+        name: "Data Library",
+        url: "#",
+        icon: (
+          <DatabaseIcon
+          />
+        ),
+      },
+      {
+        name: "Reports",
+        url: "#",
+        icon: (
+          <FileChartColumnIcon
+          />
+        ),
+      },
+      {
+        name: "Word Assistant",
+        url: "#",
+        icon: (
+          <FileIcon
+          />
+        ),
+      },
+    ],
+  }
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -198,7 +234,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        {pending ? (
+          <div className="flex flex-row gap-2">
+            <Skeleton className="h-10 w-1/6 rounded-full" />
+            <Skeleton className="h-10 w-5/6 rounded-md" />
+          </div>
+        ) : (
+          <NavUser user={data.user} />
+        )}
       </SidebarFooter>
     </Sidebar>
   )
