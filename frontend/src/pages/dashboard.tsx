ee1330854f6098afebbf75139c9cf301c8f150df
Jeet <jeetnath2110@gmail.com>
2026-05-03 17:19:25 +0530

    dashboard done initally

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -3,18 +3,59 @@ import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 import data from "@/app/dashboard/data.json"
 import { DashboardLayout } from "@/comon/dashboardLayout"
+import { useEffect, useState } from "react"
+import apiService from "@/comon/api/apiService"
+import { toast } from "sonner"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export const Dashboard = () => {
+    type User = {
+        firstName?: string;
+        lastName?: string;
+        email?: string;
+        role?: string;
+    };
+
+    const [userData, setUserData] = useState<User | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const fetchUser = async () => {
+        try {
+            setIsLoading(true);
+            const res = await apiService.get("/user/me");
+            setUserData(res.data.data);
+        } catch (error) {
+            toast.error("Failed to fetch user data", { position: "top-right", richColors: true });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
     return (
         <DashboardLayout>
             <div className="flex flex-1 flex-col">
                 <div className="@container/main flex flex-1 flex-col gap-2">
                     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                         <SectionCards />
-                        <div className="px-4 lg:px-6">
-                            <ChartAreaInteractive />
-                        </div>
-                        <DataTable data={data} />
+                        {isLoading &&
+                            <>
+                                <div className="p-5 flex flex-col gap-6">
+                                    <Skeleton className="h-90 w-full rounded-md" />
+                                    <Skeleton className="h-80 w-full rounded-md" />
+                                </div>
+                            </>
+                        }
+
+                        {!isLoading && userData?.role?.toLowerCase() === "hr" && (
+                            <>
+                                <div className="px-4 lg:px-6">
+                                    <ChartAreaInteractive />
+                                </div>
+                                <DataTable data={data} />
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
