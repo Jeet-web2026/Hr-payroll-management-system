e9e666c5fa196229cad51889c6e51d01145ced00
Jeet <jeetnath2110@gmail.com>
2026-05-31 10:17:35 +0530

    permission management changes

--- frontend/src/pages/management/permission-management.tsx
+++ frontend/src/pages/management/permission-management.tsx
@@ -1,9 +1,30 @@
 import { DashboardLayout } from "@/comon/dashboardLayout"
+import { Card, CardContent } from "@/components/ui/card"
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 export const PermissionManagement = () => {
   return (
     <DashboardLayout sideHeader="Permission Management">
-        <h1></h1>
+      <div className="p-5">
+        <Card>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow className="border text-base">
+                  <TableHead className="border-r">Name</TableHead>
+                  <TableHead className="border-r">Email</TableHead>
+                  <TableHead className="border-r">Role</TableHead>
+                  <TableHead className="border-r">Designation</TableHead>
+                  <TableHead className="text-center">Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
     </DashboardLayout>
   )
 }
