923aa530908d2eb106e46909c35bcac06d24e492
Jeet <jeetnath2110@gmail.com>
2026-05-31 12:27:23 +0530

    remove cache

--- backend/src/modules/users/controller/users.controller.ts
+++ backend/src/modules/users/controller/users.controller.ts
@@ -13,7 +13,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from '../../../comon/decorators/get-user.decorator';
 import type { JwtUser } from '../../../comon/decorators/get-user.decorator';
 import { UserResponseDto } from '../../../comon/dto/auth/userResponse.dto';
-import { User, UserRole } from '../model/user.entity';
+import { User } from '../model/user.entity';
 import { PaginatedResponse } from '../../../comon/interfaces/paginatedDataresponse.interface';
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 import * as express from 'express';
@@ -23,8 +23,6 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get('me')
-  @UseInterceptors(CacheInterceptor)
-  @CacheTTL(50 * 10 * 1000)
   @UseGuards(AuthGuard('jwt'))
   getProfile(@GetUser() user: JwtUser): Promise<UserResponseDto> {
     return this.usersService.findById(user.id);
