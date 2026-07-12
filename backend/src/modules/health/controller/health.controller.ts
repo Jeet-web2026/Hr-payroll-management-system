b5e33f51689be3a4bb03de21d85e49e84279da04
Jeet <jeetnath2110@gmail.com>
2026-07-12 16:07:16 +0530

    healthcheck done

--- backend/src/modules/health/controller/health.controller.ts
+++ backend/src/modules/health/controller/health.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import {
   HealthCheck,
   HealthCheckService,
@@ -18,6 +18,44 @@ export class HealthController {
 
   @Get()
   @HealthCheck()
+  @ApiOperation({
+    summary: 'Check API health status',
+    description:
+      'Verifies the health and availability of the application by returning its current operational status. This endpoint can be used by monitoring systems, load balancers, and administrators to ensure the API is running and responsive.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Request successful',
+    example: {
+      success: true,
+      statusCode: 200,
+      message: 'Request successful',
+      data: {
+        status: 'ok',
+        info: {
+          'nestjs-docs': {
+            status: 'up',
+          },
+          db: {
+            status: 'up',
+          },
+        },
+        error: {},
+        details: {
+          'nestjs-docs': {
+            status: 'up',
+          },
+          db: {
+            status: 'up',
+          },
+        },
+      },
+      meta: null,
+      path: '/api/health',
+      method: 'GET',
+      timestamp: '2026-07-12T10:36:51.120Z',
+    },
+  })
   check() {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
